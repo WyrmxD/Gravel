8aeb5b20650a76f23be23f2b2a1b28e0746b352b
Iñaki Torres <wyrmxd@gmail.com>
2014-12-10 17:34:56 +0100

    Added html_primitives and first template

---
+++ app/assets/javascripts/templates/boulder_create_div.js
@@ -0,0 +1,17 @@
+var boulder_create_div_tpl = '\
+<div class="row">\
+	<div class="col-xs-6 col-md-9">\
+		<div class="icon_big">\
+		<span class="glyphicon glyphicon-camera icon_big" aria-hidden="true" id="camera_picture"></span>\
+		</div>\
+	</div>\
+	<div class="col-xs-6 col-md-9">\
+		<div class="icon_big">\
+			<span class="glyphicon glyphicon-folder-open icon_big" aria-hidden="true" id="local_picture"></span>\
+		</div>\
+	</div>\
+</div>\
+\
+<div id="details"></div>\
+<div id="progress"></div>\
+';
