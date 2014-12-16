b453e639ac64b6bb398eaf576319187e17b15c0c
Iñaki Torres <wyrmxd@gmail.com>
2014-12-16 19:08:25 +0100

    Added map to boulder view

--- app/assets/javascripts/lib/read_boulder.js
+++ app/assets/javascripts/lib/read_boulder.js
@@ -5,12 +5,35 @@ var ReadBoulder = {};
 
 	var PICTURE_PATH = "uploads/"
 	var THUMBNAIL_PATH = "uploads/thumbs/"
+	var boulder;
 
-	ns.load_boulder = function(boulder){
+	ns.load_boulder = function(myBoulder){
+		boulder = myBoulder;
 		console.log(boulder);
 		$('#boulder_picture').prop('src', PICTURE_PATH + boulder.picture);
 		$('.boulder_read_name').append(boulder.name);
 		$('.boulder_read_location').append(boulder.location);
+		async_map_load(boulder);
 	}
+
+	function async_map_load(){
+		var maps_api_url = 'https://maps.googleapis.com/maps/api/js?v=3.exp&';
+		var callback_url = 'callback=ReadBoulder.external_map_js_success';
+		var script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src = maps_api_url + callback_url;
+		document.body.appendChild(script);
+	}
+
+	ns.external_map_js_success = function(){
+		console.log('API cargada');
+		var mapOptions = {
+			zoom: 13,
+			center: new google.maps.LatLng(boulder.latitude, boulder.longitude)
+		};
+
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	}
+
 
 }(ReadBoulder));
--- app/assets/javascripts/templates/boulder_read_div.js
+++ app/assets/javascripts/templates/boulder_read_div.js
@@ -1,11 +1,14 @@
 var boulder_read_div_tpl = '\
 <div id="boulder_read" class="row">\
-	<div class="col-xs-12 col-sm-6 col-md-5 col-lg-3 boulder_picture_col">\
+	<div class="col-xs-12 col-sm-6 col-md-5 col-lg-3 boulder_picture_col no_padding">\
 		<img id="boulder_picture" class="boulder_picture" src="">\
 	</div>\
-	<div class="col-xs-12 col-sm-6 col-md-7 col-ld-9 boulder_info_col">\
+	<div class="col-xs-12 col-sm-6 col-md-7 col-ld-9 boulder_info_col no_padding">\
 		<p class="boulder_read_name"></p>\
 		<p class="boulder_read_location"></p>\
+	</div>\
+	<div class="col-xs-12 no_padding">\
+		<div id="map-canvas" class="map-canvas"></div>\
 	</div>\
 </div>\
 ';
