9aa7ca95db95fb86c41e86aa68681f1064b64177
Iñaki Torres <wyrmxd@gmail.com>
2014-12-22 11:11:21 +0000

    Trying AJAX testing, not working

--- spec/javascripts/apiSpec.js
+++ spec/javascripts/apiSpec.js
@@ -1,21 +1,39 @@
 describe("API", function() {
 
-	beforeEach(function() {
-		jasmine.Ajax.install();
+	var request;
+	var onSuccess;
+	var onFailure;
+
+	// beforeEach(function() {
+	// 	jasmine.Ajax.install();
+	// });
+
+	// afterEach(function() {
+	// 	jasmine.Ajax.uninstall();
+	// });
+
+	describe("on success", function() {
+
+
+		it("calls onSuccess", function() {
+			onSuccess = jasmine.createSpy('onSuccess');
+			onFailure = jasmine.createSpy('onFailure');
+
+			API.get("boulder.api_boulder_path", onSuccess);
+
+			request = jasmine.Ajax.requests.mostRecent();
+			// request.respondWith(TestResponses.get.success);
+
+			// expect(request.url).toBe('/api/boulder');
+			// expect(onSuccess).not.toHaveBeenCalled();
+			// expect(request.method).toBe('GET');
+
+
+			// //expect(onSuccess).toHaveBeenCalledWith('awesome response');
+			expect(onSuccess).toHaveBeenCalled();
+		});
+
 	});
 
-	afterEach(function() {
-		jasmine.Ajax.uninstall();
-	});
-
-	it("should be able to perform a simple get", function() {
-		var doneFn = jasmine.createSpy("success");
-		API.get("boulder.api_boulder_path", doneFn);
-		
-		expect(jasmine.Ajax.requests.mostRecent().url).toBe('/api/boulder');
-		expect(doneFn).not.toHaveBeenCalled();
-
-
-	});
 
 });
